6e35cddf2744ebcc08ae0c824eb0d980c1fc3512
carlos8657 <carlos865@outlook.com>
2023-02-27 17:55:19 -0700

    Formulario

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
+const misRutas = require('./router/index');
+const path = require('path');
 
-var app = express();
-var port = 500;
 
-// Convierte una petición recibida (POST-GET...) a objeto JSON
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+const app = express()
+const { renderFile } = require('ejs')
 
-app.get('/', function(req, res){
-	res.status(200).send({
-		message: 'Escuchando el puerto 500'
-	});
-});
+app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
+app.engine('html',require('ejs').renderFile)
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(misRutas);
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(__dirname + '/public/error.html');
+})
+
+const port = 500
 app.listen(port,()=>{
-    console.log('Inciado el puerto 500');
+    console.log('Inciado el puerto 500')
 })
 
 
---
+++ router/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require ('body-parser');
+
+router.get('/formulario',(req,res)=>{
+    const valores ={
+        valor:req.query.valor,
+        pInicial:req.query.pInicial,
+        plazos: req.query.plazos
+    }
+    res.render('cotizacion.html',valores)
+
+})
+
+router.post('/formulario',(req,res)=>{
+    const valores ={
+        valor:req.body.valor,
+        pInicial:req.body.pInicial,
+        plazos: req.body.plazos
+    }
+    res.render('cotizacion.html',valores)
+})
+
+module.exports=router;
